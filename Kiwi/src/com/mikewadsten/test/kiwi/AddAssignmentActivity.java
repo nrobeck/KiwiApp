161742eadbe0af9c9c1a2590c078ec43b20e4cad
Mike Wadsten <mikewadsten@gmail.com>
2013-11-01 16:24:19 -0500

    Create AddAssignmentActivity, add date time picker example.

---
+++ Kiwi/src/com/mikewadsten/test/kiwi/AddAssignmentActivity.java
@@ -0,0 +1,68 @@
+package com.mikewadsten.test.kiwi;
+
+import com.cocosw.undobar.UndoBarController;
+import com.cocosw.undobar.UndoBarController.UndoListener;
+import com.mikewadsten.test.kiwi.DoneBar.DoneButtonHandler;
+import com.mikewadsten.test.kiwi.assignment.AssignmentUtils;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class AddAssignmentActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_add_course);
+		
+		final Activity activity = this;
+		
+		Utils.makeActionBarDoneButton(getActionBar(), new DoneButtonHandler() {
+            
+            @Override
+            public void onDone(View view) {
+                AssignmentUtils.showTimeEditDialog(getFragmentManager(), null, null);
+                UndoBarController.show(activity, "New assignment created.", new UndoListener() {
+                    @Override
+                    public void onUndo(Parcelable token) {
+                        Toast.makeText(activity, "Undone.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+	}
+
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	    getMenuInflater().inflate(R.menu.cancel, menu);
+	    return true;
+    }
+
+    @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			finish();
+			return true;
+		case R.id.cancel:
+		    UndoBarController.show(this, "Assignment deleted.", new UndoListener() {
+		        @Override
+		        public void onUndo(Parcelable token) {
+		            Toast.makeText(getApplication(), "Assignment not deleted.", Toast.LENGTH_SHORT).show();
+		        }
+		    });
+		}
+		// TODO Auto-generated method stub
+		return super.onOptionsItemSelected(item);
+	}
+	
+	
+
+}
---
+++ Kiwi/src/com/mikewadsten/test/kiwi/assignment/AssignmentUtils.java
@@ -0,0 +1,39 @@
+package com.mikewadsten.test.kiwi.assignment;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+
+import com.android.datetimepicker.time.TimePickerDialog;
+import com.mikewadsten.test.kiwi.Assignment;
+
+public class AssignmentUtils {
+	// Implementation based on AlarmUtils.showTimeEditDialog from the AOSP DeskClock app
+	public static void showTimeEditDialog(FragmentManager manager,
+			final Assignment assignment, TimePickerDialog.OnTimeSetListener listener) {
+		int hour, minutes;
+		
+		if (assignment == null) {
+			hour = 0;
+			minutes = 0;
+		} else {
+			hour = assignment.getId();
+			minutes = assignment.getId();
+		}
+		
+		TimePickerDialog dialog = TimePickerDialog.newInstance(listener, hour, minutes, false);
+		dialog.setThemeDark(false);
+		
+		manager.executePendingTransactions();
+		final FragmentTransaction ft = manager.beginTransaction();
+		final Fragment prev = manager.findFragmentByTag("TIMEPICK");
+		if (prev != null) {
+			ft.remove(prev);
+		}
+		ft.commit();
+		
+		if (dialog != null && !dialog.isAdded()) {
+			dialog.show(manager, "TIMEPICK");
+		}
+	}
+}
