a9ee6bce39b0fcab7ad1afd3a2d160f616a5dd14
Jared Poetter <poet0007@umn.edu>
2013-11-05 08:58:48 -0600

    Created edit_assignment_fragment.xml & add_assignment_layout.xml. Theses both are going to be used for both editing and adding a new assignment. Also I cleaned up Kiwi Icon so there is no more of the white background.

--- Kiwi/src/umn/cs5115/kiwi/fragments/EditAssignmentFragment.java
+++ Kiwi/src/umn/cs5115/kiwi/fragments/EditAssignmentFragment.java
@@ -1,7 +1,22 @@
 package umn.cs5115.kiwi.fragments;
 
+import umn.cs5115.kiwi.R;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class EditAssignmentFragment extends Fragment {
 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+//		return inflater.inflate(R.layout.overview_fragment, null);
+		return inflater.inflate(R.layout.edit_assignment_fragment, null);
+		// TODO Auto-generated method stub
+		//return super.onCreateView(inflater, container, savedInstanceState);
+	}
+	
 }
