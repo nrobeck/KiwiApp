86e79bcc7a73935df1dfd611ec9cd588ea56aee7
Jared Poetter <poet0007@umn.edu>
2013-11-22 16:55:46 -0600

    Added the time and date picker to the add/edit course page.

--- Kiwi/src/umn/cs5115/kiwi/fragments/EditCourseFragment.java
+++ Kiwi/src/umn/cs5115/kiwi/fragments/EditCourseFragment.java
@@ -1,10 +1,21 @@
 package umn.cs5115.kiwi.fragments;
 
+import java.util.Calendar;
+import java.util.Locale;
+
+import com.android.datetimepicker.date.DatePickerDialog;
+import com.android.datetimepicker.date.DatePickerDialog.OnDateSetListener;
+import com.android.datetimepicker.time.RadialPickerLayout;
+import com.android.datetimepicker.time.TimePickerDialog.OnTimeSetListener;
+
 import umn.cs5115.kiwi.Course;
 import umn.cs5115.kiwi.DatabaseHandler;
 import umn.cs5115.kiwi.EditAssignmentActivity;
 import umn.cs5115.kiwi.EditCourseActivity;
 import umn.cs5115.kiwi.R;
+import umn.cs5115.kiwi.assignment.AssignmentUtils;
+import umn.cs5115.kiwi.ui.DateButton;
+import umn.cs5115.kiwi.ui.TimeButton;
 import umn.cs5115.kiwi.ui.DoneBar.DoneBarListenable;
 import umn.cs5115.kiwi.ui.DoneBar.DoneBarListener;
 import android.app.Activity;
@@ -14,6 +25,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -22,7 +34,90 @@ public class EditCourseFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.edit_course_fragment, container);
+		View layout = inflater.inflate(R.layout.edit_course_fragment, container);
+		
+		Calendar current = Calendar.getInstance(Locale.US);
+		final int year = current.get(Calendar.YEAR),
+				month = current.get(Calendar.MONTH),
+				day = current.get(Calendar.DAY_OF_MONTH),
+				hour = current.get(Calendar.HOUR_OF_DAY),
+				minute = current.get(Calendar.MINUTE);
+		final int incrementedMonth = month + 2;
+		final int incrementedHour = hour + 1;
+		
+		final DateButton startDB = (DateButton)layout.findViewById(R.id.start_date_button);
+		final DateButton endDB = (DateButton)layout.findViewById(R.id.end_date_button);
+		
+		//Start Course Date
+		startDB.setDate(year, month, day);
+		
+		startDB.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				AssignmentUtils.showDateEditDialog(getFragmentManager(), year, month, day, new OnDateSetListener() {
+					@Override
+					public void onDateSet(DatePickerDialog dialog, int year, int monthOfYear,
+							int dayOfMonth) {
+						startDB.setDate(year, monthOfYear, dayOfMonth);
+					}
+				});
+			}
+		});
+		
+		//End Course Date
+		//Increment the end course dialog by 2 months by default
+		endDB.setDate(year, incrementedMonth, day); //This is why there is 2 months difference between pickers
+		
+		endDB.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				AssignmentUtils.showDateEditDialog(getFragmentManager(), year, incrementedMonth, day, new OnDateSetListener() {
+					@Override
+					public void onDateSet(DatePickerDialog dialog, int year, int monthOfYear,
+							int dayOfMonth) {
+						endDB.setDate(year, monthOfYear, dayOfMonth);
+					}
+				});
+			}
+		});		
+		
+		final TimeButton startTB = (TimeButton)layout.findViewById(R.id.start_time_button);
+		final TimeButton endTB = (TimeButton)layout.findViewById(R.id.end_time_button);
+		
+		//Start Course Time
+		startTB.setTime(hour, minute);
+
+		startTB.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				AssignmentUtils.showTimeEditDialog(getFragmentManager(), hour, minute, new OnTimeSetListener() {
+
+					@Override
+					public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
+						startTB.setTime(hourOfDay, minute);
+					}
+				});
+			}
+		});
+		
+		//End Course Time
+		//Increment the end course time by 1 hour by default
+		endTB.setTime(incrementedHour, minute); //This is why there is 1 hour difference between pickers 
+
+		endTB.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				AssignmentUtils.showTimeEditDialog(getFragmentManager(), incrementedHour, minute, new OnTimeSetListener() {
+
+					@Override
+					public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
+						endTB.setTime(hourOfDay, minute);
+					}
+				});
+			}
+		});
+		
+		return layout;
 	}
 
     @Override
